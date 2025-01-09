c337cbf5f5772af77b49ab1f8608d9708921025f
Yoshi <andreolienzo@gmail.com>
2025-01-09 11:29:11 +0000

    Fix

--- manage/docker-entrypoint.sh
+++ manage/docker-entrypoint.sh
@@ -4,5 +4,5 @@
 # Démarrer le serveur Gmod en arrière-plan
 /gmod/srcds_run -game garrysmod +maxplayers 16 +map gm_construct >> /gmod/gmod_console.log 2>&1 &
 
-# Lancer Apache en premier plan
-apache2-foreground
+# Lancer Nginx en premier plan
+exec nginx -g 'daemon off;'
